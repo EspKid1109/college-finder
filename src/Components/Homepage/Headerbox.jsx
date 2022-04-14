36b20d1758a2a1036e7df18e2332b750220fcf03
EspKid1109 <alex.chase@ufl.edu>
2022-04-14 18:00:24 -0400

    Added SubmitPage stuff

--- src/Components/Homepage/Headerbox.jsx
+++ src/Components/Homepage/Headerbox.jsx
@@ -17,7 +17,7 @@ class Headerbox extends Component {
           >
             <Box sx={{ p: 2 }}>
               <Typography variant="h2" align="justify">
-                College Finder: The Floobin Doobin.
+                College Finder: Exotic.
               </Typography>
             </Box>
           </Grid>
--- src/Components/SubmitBody.jsx
+++ src/Components/SubmitBody.jsx
@@ -1,71 +1,39 @@
-import React, { Component } from "react";
+import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import Headerbox from "./Homepage/Headerbox";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import SideBox from "./Homepage/Sidebox";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'id', headerName: 'College', width: 230 },
+  { field: 'in', headerName: 'InStateCost', width: 120 },
+  { field: 'out', headerName: 'OutState Cost', width: 120 },
+  { field: 'ar', headerName: 'Acceptance Rate', width: 130 },
+  { field: 'top', headerName: 'Top 3 Majors', width: 260 },
+  { field: 'gpa', headerName: 'GPA', width: 60 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 'University of South Florida', in: '$6,381', out: '$28,659', ar: '38.8%', top: 'Engineering,BMM,Bio', gpa: '4.42'},
 ];
 
-class SubmitBody extends Component {
-  state = {};
-
-  render() {
-    return (
-      <Box
-        sx={{ flexGrow: 1, p: 2 }}
-        height="75vh"
-        width="120vh"
-        position="relative"
-      >
-            <div style={{ height: 400, width: '100%' }}>
-    </div>
-        <h1>SUBMIT PAGE</h1>
-        <DataGrid
+export default function DataTable() {
+  return (
+    <Box
+      style={{background: 'white'}}
+      sx={{ flexGrow: 1, border: 1, p: 2 }}
+      height="75vh"
+      width="120vh"
+      position="relative"
+    >
+      <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        checkboxSelection
       />
-      </Box>
-      
-    );
-  }
+    </Box>
+  );
 }
 
-export default SubmitBody;
